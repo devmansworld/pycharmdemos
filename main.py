1c99543b89d540064c1f881e3533319e353b5433
Leo PD <leonardopachecoduran@gmail.com>
2023-09-21 22:28:37 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,84 @@
-import pyautogui
-import math
+import os
 import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Set the radius of the circle and the number of steps
-radius = 100  # You can adjust this value
-num_steps = 360  # 360 degrees for a full circle
+# Set the directory containing the Microsoft Edge WebDriver executable
+webdriver_dir = os.path.expanduser("~")  # Use the user's home directory
 
-# Calculate the step size to move in a circle
-step_size = (2 * math.pi * radius) / num_steps
+# Add the WebDriver directory to the PATH environment variable
+os.environ["PATH"] += os.pathsep + webdriver_dir
 
-# Center of the circle (you can adjust these coordinates)
-center_x, center_y = 500, 500  # Adjust as needed
+# Initialize the Edge WebDriver (without specifying executable_path)
+driver = webdriver.Edge()
 
-# Slow down the mouse movement by adding a delay between steps
-delay_between_steps = 0.1  # Adjust as needed
+# Open the convenience store website
+driver.get('https://www.homedepot.com.mx')
 
-# Move the mouse in a clockwise circle
-for i in range(num_steps):
-    angle = i * step_size
-    x = center_x + radius * math.cos(angle)
-    y = center_y + radius * math.sin(angle)
+print("Opened website")
 
-    # Move the mouse to the calculated coordinates with a duration of 0.1 seconds (adjust as needed)
-    pyautogui.moveTo(x, y, duration=0.1)
+# Perform the shopping cart operations
+products = ['pan', 'pen', 'apple']
 
-    # Add a delay between steps to control the speed (adjust as needed)
-    time.sleep(delay_between_steps)
+# Search for items
+for product in products:
+    search_input = driver.find_element(By.ID, 'SimpleSearchForm_SearchTerm')
+    search_input.clear()
+    search_input.send_keys(product)
+    search_input.send_keys(Keys.RETURN)
 
-    # Print a message for each step performed
-    print(f"Step {i + 1}: Moved to ({x}, {y})")
+    try:
+        # Wait for the 'btn1' element to be present for a maximum of 5 seconds
+        element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'btn1'))
+        )
 
-# Move the mouse back to the original position (optional)
-pyautogui.moveTo(center_x, center_y, duration=0.1)
-print("Mouse movement completed.")
+        # Perform actions on the element once it's found (e.g., click)
+        element.click()
+
+        # Wait for 4 seconds (you can adjust the duration)
+        time.sleep(4)
+
+    except Exception as e:
+        # Handle any exceptions (e.g., element not found, timeout)
+        print(f"An error occurred: {str(e)}")
+
+# Navigate to the cart page
+#cart_link = driver.find_element(By.ID, 'link-cart')
+#cart_link.click()
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(2)
+goto_cart_button = driver.find_element(By.ID, 'GotoCartButton2')
+goto_cart_button.click()
+# Wait for 3 seconds (you can adjust the duration)
+
+# Continue with checkout and other steps as needed
+time.sleep(50)
+# Parse the cart page using BeautifulSoup
+cart_page_html = driver.page_source
+soup = BeautifulSoup(cart_page_html, 'html.parser')
+
+# Find and extract the total value
+total_element = soup.find('span', {'class': 'summary__text-value summary__text-value--price'})
+    # Find and extract the total value
+
+
+
+if total_element:
+    cart_total = total_element.text.strip()
+    print(f"Cart Total: {cart_total}")
+else:
+    print("Total element not found!")
+
+
+
+
+
+
+# Close the browser
+driver.quit()
+
